06e32b5c3b7d29bf4fd61b813273774f613373a7
franPzTo <franperezt1@gmail.com>
2025-10-27 21:50:45 -0300

    uso de useState y button, falta la funcion

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+function HamburgerMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="relative">
+      {/* Botón de Hamburguesa */}
+      <button 
+        onClick={() => setIsOpen(!isOpen)} 
+        className="fixed top-4 right-4 z-50 p-2 focus:outline-none focus:ring-2 focus:ring-gray-300 rounded-md lg:hidden" // Ocultar en pantallas grandes
+      >
+        <div className={`w-6 h-0.5 bg-black mb-1.5 transition-all duration-300 ${isOpen ? 'rotate-45 translate-y-2' : ''}`}></div>
+        <div className={`w-6 h-0.5 bg-black mb-1.5 transition-all duration-300 ${isOpen ? 'opacity-0' : ''}`}></div>
+        <div className={`w-6 h-0.5 bg-black transition-all duration-300 ${isOpen ? '-rotate-45 -translate-y-2' : ''}`}></div>
+      </button>
+
+      
+    </div>
+  );
+}
+
+export default HamburgerMenu;
