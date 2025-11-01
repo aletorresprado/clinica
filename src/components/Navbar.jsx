39dd1abe9bac8ebacbef89a18d9405da2c6d7df9
franPzTo <franperezt1@gmail.com>
2025-11-01 15:52:15 -0300

    Correción de error y terminado

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,57 +1,59 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import img from '../assets/Logo-Clinica.png';
+import { Link } from 'react-router-dom';
 
-function HamburgerMenu() {
-  const [isOpen, setIsOpen] = useState(false);
+function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
 
-  return (
-    <div className="relative">
-      {/* Botón de Hamburguesa */}
-      <button 
-        onClick={() => setIsOpen(!isOpen)} 
-        className="fixed top-4 right-4 z-50 p-2 focus:outline-none focus:ring-2 focus:ring-gray-300 rounded-md lg:hidden" // Ocultar en pantallas grandes
-      >
-        <div className={`w-6 h-0.5 bg-black mb-1.5 transition-all duration-300 ${isOpen ? 'rotate-45 translate-y-2' : ''}`}></div>
-        <div className={`w-6 h-0.5 bg-black mb-1.5 transition-all duration-300 ${isOpen ? 'opacity-0' : ''}`}></div>
-        <div className={`w-6 h-0.5 bg-black transition-all duration-300 ${isOpen ? '-rotate-45 -translate-y-2' : ''}`}></div>
-      </button>
+    return (
+        <header className="relative bg-red-200 shadow-md" id="inicio">
+            <div className='flex items-center justify-between h-20 px-5'>
+                <div className='flex items-center'>
+                    <img src={img} alt="Clinica San Miguel" className="h-14 mr-5" />
+                    <h2 className="text-red-600 text-2xl font-bold hidden lg:block">Clinica San Miguel</h2>
+                    <h2 className="text-red-600 text-2xl font-bold block lg:hidden">Clinica S.M.</h2>
+                </div>
+                <nav className='hidden lg:flex items-center p-2'>
+                    <Link to="/login" className="mx-3 font-bold text-red-600 hover:text-sky-600">Iniciar Sesión</Link>
+                    <Link to="/register" className="mx-3 font-bold text-red-600 hover:text-sky-600">Registro</Link>
+                    <Link to="#profecionales" className="mx-3 font-bold text-red-600 hover:text-sky-600">Profesionales</Link>
+                    <Link to="/Clinica-SanMiguel" className="mx-3 font-bold text-red-600 hover:text-sky-600">Acerca de</Link>
+                    <Link to="#contacto" className="mx-3 font-bold text-red-600 hover:text-sky-600">Contacto</Link>
+                </nav>
+                <button 
+                    onClick={() => setIsOpen(!isOpen)} 
+                    className="p-2 focus:outline-none focus:ring-2 focus:ring-red-500 rounded-md lg:hidden"
+                >
+                    <div className={`w-6 h-0.5 bg-red-500 mb-1.5 transition-all duration-300 ${isOpen ? 'rotate-45 translate-y-2' : ''}`}></div>
+                    <div className={`w-6 h-0.5 bg-red-500 mb-1.5 transition-all duration-300 ${isOpen ? 'opacity-0' : ''}`}></div>
+                    <div className={`w-6 h-0.5 bg-red-500 transition-all duration-300 ${isOpen ? '-rotate-45 -translate-y-2' : ''}`}></div>
+                </button>
+            </div>
 
-      {/* Función Condicional del Botón*/}
-      {isOpen && (
-        <div 
-          onClick={() => setIsOpen(false)} 
-          className="fixed inset-0 bg-green-300 bg-opacity-25 z-40 lg:hidden" // Ocultar en pantallas grandes
-        ></div>
-      )}
+            {isOpen && (
+                <div 
+                    onClick={() => setIsOpen(false)} 
+                    className="fixed inset-0 bg-red-300 bg-opacity-50 z-40 lg:hidden" 
+                ></div>
+            )}
 
-      {/* Panel Lateral del Menú */}
-      <div 
-        className={`fixed top-0 right-0 w-64 h-full bg-green-200 shadow-lg transform transition-transform duration-300 ease-in-out z-50 lg:hidden
-          ${isOpen ? 'translate-x-0' : 'translate-x-full'}`} // Si está abierto, no se mueve; si está cerrado, se mueve fuera de pantalla
-      >
-        <div className="p-4 flex flex-col items-center">
-          <h2 className="text-xl font-bold mb-4">Menú</h2>
-          <ul className='flex flex-col items-center'>
-            {/* Charlar para cambar las "a" por Link */}
-            <li className="mb-2"><a href="#" className="mx-3 font-bold text-green-600 hover:text-sky-600">Iniciar Sesión</a></li> 
-            <li className="mb-2"><a href="#" className="mx-3 font-bold text-green-600 hover:text-sky-600">Registro</a></li>
-            <li className="mb-2"><a href="#" className="mx-3 font-bold text-green-600 hover:text-sky-600">Profecionales</a></li>
-            <li className="mb-2"><a href="#" className="mx-3 font-bold text-green-600 hover:text-sky-600">Acerca de</a></li>
-            <li className="mb-2"><a href="#" className="mx-3 font-bold text-green-600 hover:text-sky-600">Contacto</a></li>
-          </ul>
-        </div>
-      </div>
+            <div 
+                className={`fixed top-0 right-0 w-64 h-full bg-red-200 shadow-lg transform transition-transform duration-500 ease-in-out z-50 lg:hidden
+                    ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}
+            >
+                <div className="p-4 flex flex-col items-center pt-20">
+                    <h2 className="text-xl font-bold mb-4">Menú</h2>
+                    <ul className='flex flex-col items-center'>
+                        <li className="mb-2"><Link to="/login" className="mx-3 font-bold text-red-600 hover:text-sky-600">Iniciar Sesión</Link></li> 
+                        <li className="mb-2"><Link to="/register" className="mx-3 font-bold text-red-600 hover:text-sky-600">Registro</Link></li>
+                        <li className="mb-2"><Link to="#profecionales" className="mx-3 font-bold text-red-600 hover:text-sky-600">Profesionales</Link></li>
+                        <li className="mb-2"><Link to="/Clinica-SanMiguel" className="mx-3 font-bold text-red-600 hover:text-sky-600">Acerca de</Link></li>
+                        <li className="mb-2"><Link to="#contacto" className="mx-3 font-bold text-red-600 hover:text-sky-600">Contacto</Link></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+    );
+};
 
-      {/* Contenido que siempre es visible en pantallas grandes */}
-      <nav className="hidden bg-green-200 lg:flex justify-center p-4">
-        {/* Cambiar los "a" por link */}
-        <a href="#" className="mx-3 font-bold text-green-600 hover:text-sky-600">Iniciar Sesión</a>
-        <a href="#" className="mx-3 font-bold text-green-600 hover:text-sky-600">Registro</a>
-        <a href="#" className="mx-3 font-bold text-green-600 hover:text-sky-600">Profecionales</a>
-        <a href="#" className="mx-3 font-bold text-green-600 hover:text-sky-600">Acerca de</a>
-        <a href="#" className="mx-3 font-bold text-green-600 hover:text-sky-600">Contacto</a>
-      </nav>
-    </div>
-  );
-}
-
-export default HamburgerMenu;
+export default Navbar;
