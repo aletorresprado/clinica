0097a48dbe4855ffddee81db85638a77c8463023
franPzTo <franperezt1@gmail.com>
2025-11-02 19:38:34 -0300

    Componente contacto terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
+import Contacto from "./components/Contacto";
+
 const App = () => {
 	return (
-		<div className="min-h-screen flex flex-col bg-black text-white/95 items-center justify-center text-2xl font-bold text-center">
-			<img
-				className="size-40"
-				src="https://res.cloudinary.com/dltj8bim0/image/upload/v1761060580/logo_kukwt0.png"
-				alt=""
-			/>
-			<p>Hello Vite + React + TailwindCSS!</p>
+		<div>
+			<Contacto />
 		</div>
 	);
 };
