f06b855b0aa79b3e84512a390f87200e74cde56d
franPzTo <franperezt1@gmail.com>
2025-11-05 21:53:20 -0300

    Componente profesional y infoDocs.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Contacto from "./components/Contacto";
+import Profesionales from "./components/Profesionales";
 
 const App = () => {
 	return (
 		<div>
+			<Profesionales />
 			<Contacto />
 		</div>
 	);
